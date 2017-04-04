ca0bd5fb42f42d92ceb9989bf966d4f24bcbe903
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-04 14:51:27 +0800

    Updated .gitignore

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$*sed%qxwwnd7-kh_$4x6yj&*r8n8km7zq2zyy19bh=&zzv!x&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,4 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
