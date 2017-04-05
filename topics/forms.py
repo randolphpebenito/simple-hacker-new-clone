7a66b51db00a91150faecb9be6f4032c06524569
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-05 18:20:44 +0800

    Updated topic field to text area.

--- topics/forms.py
+++ topics/forms.py
@@ -1,4 +1,4 @@
 from django import forms
     
 class CreateTopicForm(forms.Form):
-    topic = forms.CharField(max_length=255, help_text='255 characters max.')
+    topic = forms.CharField(max_length=255, widget=forms.Textarea, help_text='255 characters max.')
