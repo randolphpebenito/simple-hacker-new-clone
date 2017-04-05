3ed9f08a0840cf62961f8b746ff9d411882991eb
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-05 13:21:42 +0800

    Declared global variable topics.

--- topics/views.py
+++ topics/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import CreateTopicForm
 
-# Create your views here.
+topics = []
+
+def home(request):
+    global topics
+    return render(request, "topics.html", {'topics': topics})
+
+def create_topic(request):
+    global topics
+    if request.method == 'POST':
+        form = CreateTopicForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            topics.append(form.cleaned_data['topic'])
+            return HttpResponseRedirect('/topic/all/')
+    else:
+        form = CreateTopicForm()
+    return render(request, "create_topic.html", { 'topics': topics[0], 'form': form })
+
+
