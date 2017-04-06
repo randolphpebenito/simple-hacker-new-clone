c7f03236b6b027c9dc21956d9c57868133161967
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-07 02:52:55 +0800

    Created redirectview for home.

--- webapp/urls.py
+++ webapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='topic/list/')),
     url(r'^admin/', admin.site.urls),
     url(r'^topic/', include('topics.urls', namespace='topic')),
 ]
