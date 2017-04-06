371848de812e70c82b6378686e249837300fa355
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-07 02:54:27 +0800

    Updated HttpResponseRedirect to redirect shortcut.

--- topics/views.py
+++ topics/views.py
@@ -1,14 +1,25 @@
+#Built-In
+from operator import attrgetter
+
+#Django Built-In
+from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+#In-App
 from .forms import CreateTopicForm
 from .topics import Topic
 
 #Global Variable topics
+#TODO: Classify TopicList
 topics = []
 
 def paginate_list(list_obj, page_req_var):
-    paginator = Paginator(list_obj, 3)
+    """
+        Pagination config
+    """
+    paginator = Paginator(list_obj, settings.ITEM_LIMIT_PER_PAGE)
     try:
         pl = paginator.page(page_req_var)
     except PageNotAnInteger:
@@ -18,20 +29,51 @@ def paginate_list(list_obj, page_req_var):
     return pl
 
 def home(request):
+    """
+        List all topics sorted by total_vote_score descending (w/ pagination)
+    """
     global topics
-    sorted_items = sorted(topics, key=lambda o: o.total_vote_count)
+    sorted_items = sorted(topics, key=attrgetter('total_vote_score'), reverse=True)
     topic_list = paginate_list(sorted_items, request.GET.get('page'))
     return render(request, "topics.html", {'topics': topic_list})
 
 def create_topic(request):
+    """
+        Creating a topic via form
+    """
     global topics
     if request.method == 'POST':
         form = CreateTopicForm(request.POST or None, request.FILES or None)
         if form.is_valid():
             t = Topic(topic=form.cleaned_data['topic'])
             topics.append(t)
-            return HttpResponseRedirect('/topic/all/')
+            return redirect('topic:home')
     else:
         form = CreateTopicForm()
     return render(request, "create_topic.html", {'form': form })
 
+def upvote_topic(request, topic_id):
+    """
+        Upvoting a topic based on topic_id
+        returns total_vote_score if topic_id exists
+        otherwise 404
+    """
+    topic_obj = next((t for t in topics if t.topic_id == topic_id), None)
+    if topic_obj is not None:
+        topic_obj.upvote(1)
+        return HttpResponse(topic_obj.total_vote_score)
+    else:
+        raise Http404("Topic does not exist")
+
+def downvote_topic(request, topic_id):
+    """
+        Downvoting a topic based on topic_id
+        returns total_vote_score if topic_id exists
+        otherwise 404
+    """
+    topic_obj = next((t for t in topics if t.topic_id == topic_id), None)
+    if topic_obj is not None:
+        topic_obj.downvote(1)
+        return HttpResponse(topic_obj.total_vote_score)
+    else:
+        raise Http404("Topic does not exist")
