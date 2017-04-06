6c743d116305f63c82ffcb97d829171084866b09
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-07 02:54:41 +0800

    Added url naming.

--- topics/urls.py
+++ topics/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import include, url
-from .views import home, create_topic 
+from .views import (
+    home, 
+    create_topic,
+    downvote_topic,
+    upvote_topic,
+)
 
 urlpatterns = [
-    url(r'^all/$', home, name='home'),
     url(r'^create/$', create_topic, name='create-topic'),
+    url(r'^list/$', home, name='home'),
+    url(r'^(?P<topic_id>[^/]+)/upvote/$', upvote_topic, name='upvote-topic'),
+    url(r'^(?P<topic_id>[^/]+)/downvote/$', downvote_topic, name='downvote-topic'),
 ]
