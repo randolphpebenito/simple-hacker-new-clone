be8f3b8b822699bdde4f3a69fbe8f9fe00334e36
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-07 12:35:52 +0800

    Created TopicList class for list of objects wrapper.

--- topics/topics.py
+++ topics/topics.py
@@ -1,8 +1,26 @@
+from operator import attrgetter
 import uuid 
+
+class TopicList(list):
+    """
+        This class is a simple list of objects wrapper.
+        w/ utility methods such as sorting and getting obj by its attributes and values
+    """
+    def __init__(self, *args, **kwargs):
+        super(TopicList, self).__init__()
+
+    def append(self, value):
+        return super(TopicList, self).append(value)
+
+    def sort_by(self, attr, reverse=False):
+        return sorted(self, key=attrgetter(attr), reverse=reverse)
+
+    def get_obj_by_attr_or_none(self, attr, val):
+        return next((o for o in self if getattr(o, attr) == val), None)
 
 class Topic(object):
     def __init__(self, topic=None, vote_score=0):
-        self.topic_id = str(uuid.uuid4())[:32].replace('-', '').lower()
+        self.topic_id = str(uuid.uuid4())[:32].replace('-', '').lower() #Generate ID for every Topic instance
         self.topic = topic
         self.vote_score = vote_score
 
